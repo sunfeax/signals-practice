c2f9815a824c5a83f9db1638614db1661db41bd8
Vlad Uski <sunfeax@gmail.com>
2026-09-03 17:35:00 +0200

    Add Prettier configuration and enhance effect session timer component with timer functionality

--- src/app/tasks/effect-session-timer/effect-session-timer.component.ts
+++ src/app/tasks/effect-session-timer/effect-session-timer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+
+type TimerState = 'started' | 'paused' | 'initial';
 
 @Component({
   selector: 'app-effect-session-timer',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './effect-session-timer.component.html',
   styleUrl: './effect-session-timer.component.css',
 })
-export class EffectSessionTimerComponent {}
+export class EffectSessionTimerComponent {
+  constructor() {
+    effect((onCleanup) => {
+      if (this.state() === 'started') {
+        const intervalId = setInterval(() => this.timer.update((v) => v + 1), 1000);
+        onCleanup(() => clearInterval(intervalId));
+      }
+    });
+  }
+
+  timer = signal<number>(0);
+  state = signal<TimerState>('initial');
+  notification = computed(() => this.timer() >= 30);
+
+  reset(): void {
+    this.timer.set(0);
+    this.state.set('initial');
+  }
+
+  pause(): void {
+    this.state.set('paused');
+  }
+
+  start(): void {
+    this.state.set('started');
+  }
+}
